c671f69b79ccfd7e5f777cbf200e81c9dda7391f
Rodrigo Fernandez <rofaccess@gmail.com>
2024-08-27 09:50:26 -0400

    Create gem

---
+++ lib/employee_week_schedule.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "employee_week_schedule/version"
+
+module EmployeeWeekSchedule
+  class Error < StandardError; end
+  # Your code goes here...
+end
